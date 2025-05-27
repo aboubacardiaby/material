c2cf5e743b9796934c5edbfa7fca0452079db92e
AbouDiaby <AbouDiaby@DESKTOP-E2K609Q>
2025-05-27 17:24:09 -0500

    add unit test

--- material.Tests/MaterialControllerTests.cs
+++ material.Tests/MaterialControllerTests.cs
@@ -36,5 +36,45 @@ namespace material.Tests
             Assert.IsNotNull(materials);
             Assert.AreEqual(2, materials.Count());
         }
+        //[Test]
+        //public void GetAll_ReturnsOkResult_WithListOfMaterials()
+        //{
+        //    // Act
+        //    var result = _controller.GetAll();
+
+        //    // Assert
+        //    Assert.IsInstanceOf<OkObjectResult>(result.Result);
+
+        //    var okResult = result.Result as OkObjectResult;
+        //    Assert.IsNotNull(okResult);
+
+        //    var materials = okResult.Value as IEnumerable<MaterialEntity>;
+        //    Assert.IsNotNull(materials);
+        //    Assert.AreEqual(2, materials.Count());
+        //}
+
+        [Test]
+        public void GetAll_ReturnsMaterials_WithExpectedProperties()
+        {
+            // Act
+            var result = _controller.GetAll();
+
+            // Assert
+            var okResult = result.Result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+
+            var materials = okResult.Value as IEnumerable<MaterialEntity>;
+            Assert.IsNotNull(materials);
+
+            var materialList = materials.ToList();
+
+            Assert.AreEqual(1, materialList[0].Id);
+            Assert.AreEqual("Material 1", materialList[0].Name);
+            Assert.AreEqual("Description for Material 1", materialList[0].Description);
+
+            Assert.AreEqual(2, materialList[1].Id);
+            Assert.AreEqual("Material 2", materialList[1].Name);
+            Assert.AreEqual("Description for Material 2", materialList[1].Description);
+        }
     }
 }
